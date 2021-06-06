ba10311317cd885aab5a8a423c44f05d6bd54b9c
venkatesh <123kamtham@gmail.com>
2021-06-06 20:44:21 +0530

    v

--- kalathreads/settings.py
+++ kalathreads/settings.py
@@ -123,3 +123,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIR = [
     os.path.join(BASE_DIR, 'mysite/static')
 ]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
