6929ebe7bb99a75338aa4a04bdfb9109d899b41f
venkatesh <123kamtham@gmail.com>
2021-06-10 14:51:40 +0530

    l

--- mysite/views.py
+++ mysite/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'mysite/home.html',{})
+
+def contact(request):
+    return render(request,'mysite/contact.html',{})
