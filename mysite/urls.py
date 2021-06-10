ad7bca33ed4b86533cc8a724a10aee155e6f4526
venkatesh <123kamtham@gmail.com>
2021-06-10 15:44:25 +0530

    l

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 
-app_name='mysite'
-
 urlpatterns = [
     path('',views.home,name='home'),
     path('contacts/',views.contacts,name="contact_us")
