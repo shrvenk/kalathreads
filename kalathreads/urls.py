b1022bbf27902cd0f5c786534a47c922d7077567
venkatesh <123kamtham@gmail.com>
2021-06-10 15:30:08 +0530

    l

--- kalathreads/urls.py
+++ kalathreads/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls import url,include
+from mysite import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('mysite.urls')),
 ]
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
