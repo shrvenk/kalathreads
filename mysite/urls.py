b4fddeb9cb6d0d03945e5ddf04c4bf03dee097b4
venkatesh <123kamtham@gmail.com>
2021-06-13 14:32:37 +0530

    k

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
-    path('contact/',views.contacts,name="contact_us"),
+    path('contacts/',views.contacts,name='contact'),
 ]
