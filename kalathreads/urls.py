b8bb5e75e5ad6958a3b96bfc09d5498443357b67
venkatesh <123kamtham@gmail.com>
2021-06-13 15:31:49 +0530

    m

--- kalathreads/urls.py
+++ kalathreads/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mysite.urls'))
 ]
-urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+# urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
