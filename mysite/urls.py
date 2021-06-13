0f3a517a8e03bbe4479db7e7d27326983a24b9cb
venkatesh <123kamtham@gmail.com>
2021-06-13 14:45:14 +0530

    m

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cnt/',views.home,name='home'),
     path('',views.cnt,name='cnt'),
+    path('cnt/',views.home,name='home')
 ]
