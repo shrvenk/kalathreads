cdf158d0922eda4c246b8e6e9eeec2c2972ec568
venkatesh <123kamtham@gmail.com>
2021-06-13 14:49:21 +0530

    m

--- mysite/views.py
+++ mysite/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 
 # Create your views here.
 def home(request):
-    return render(request,'mysite/home.html',{})
+    return render(request,'mysite/contact.html',{})
 
 def cnt(request):
-    return render(request,'mysite/contact.html',{})
+    return render(request,'mysite/home.html',{})
